a0daff302d31f28e441bbc5e6fa0ac9660043cf9
Ricartho Franck <ricartho.franck@esih.edu>
2019-10-01 16:27:55 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -21,6 +21,7 @@ export default class Matrix extends Component {
   
 }
  Matrix.defaultProps = {
-    const arrayBase = ["#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00"];
-    return (new Array(10).fill(arrayBaserray))
+   values: ()=>{
+        const arrayBase = ["#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00", "#F00"];
+        return (new Array(10).fill(arrayBaserray))
 }
